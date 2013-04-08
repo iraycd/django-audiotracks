fdfa53ed28c4b4487379aac20684442ed2ac79a0
Alex Marandon <contact@alexmarandon.com>
2013-04-08 22:03:42 +0200

    Prepare v0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-audiotracks',
-    version='0.2.1',
+    version='0.2.2',
     description='A pluggable Django app to publish audio tracks',
     long_description=open("README.rst").read(),
     keywords='django audio sound',
